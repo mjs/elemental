7e2b75cbb7177fb5e22d241da7994111f1d675c1
Menno Smits <menno@freshfoo.com>
2012-01-19 23:51:25 +0000

    reasonable test coverage of the stuff that currently mostly works

--- transpose-funcargs-tests.el
+++ transpose-funcargs-tests.el
@@ -1,8 +1,116 @@
-(defun plain-thing-at-point (thing-type)
-  "Like thing-at-point, but strip out any text properties"
-  (let ((thing (thing-at-point thing-type)))
-      (set-text-properties 0 (length thing) nil thing)
-      thing))
+(require 'ert)
+(require 'transpose-funcargs)
+
+; XXX with point inside an arg
+; XXX with point inside a string arg
+; XXX nested parens
+; XXX single quotes don't work in fundamental mode (but should in Python mode)
+; XXX multi-line tests
+; XXX more tests at limits
+; XXX test when not inside parens
+
+;; forward-one-funcarg
+
+(ert-deftest test-move-to-end-of-arg ()
+  (test-in-buffer "foo(|abc, def)"
+                  'forward-one-funcarg
+                  "foo(abc|, def)"))
+
+(ert-deftest test-move-to-next-arg ()
+  (test-in-buffer "foo(abc|, def)"
+                  'forward-one-funcarg
+                  "foo(abc, def|)"))
+
+(ert-deftest test-move-to-next-arg-with-trailing-space ()
+  (test-in-buffer "foo(abc|, def )"
+                  'forward-one-funcarg
+                  "foo(abc, def| )"))
+
+(ert-deftest test-move-to-end-of-arg-complex ()
+  (test-in-buffer "foo(123, |yyy=\"a,b\")"
+                  'forward-one-funcarg
+                  "foo(123, yyy=\"a,b\"|)"))
+
+(ert-deftest test-move-to-next-arg-complex ()
+  (test-in-buffer "foo(123|, yyy=\"a,b\")"
+                  'forward-one-funcarg
+                  "foo(123, yyy=\"a,b\"|)"))
+
+(ert-deftest test-move-to-next-arg-complex ()
+  (test-in-buffer "foo(123|, yyy=\"a,b\")"
+                  'forward-one-funcarg
+                  "foo(123, yyy=\"a,b\"|)"))
+
+
+;; backward-one-funcarg
+
+(ert-deftest test-move-back-to-start-of-arg ()
+  (test-in-buffer "foo(abc, def|)"
+                  'backward-one-funcarg
+                  "foo(abc, |def)"))
+
+(ert-deftest test-move-to-previous-arg ()
+  (test-in-buffer "foo(abc, |def)"
+                  'backward-one-funcarg
+                  "foo(|abc, def)"))
+
+(ert-deftest test-move-to-previous-arg-with-leading-space ()
+  (test-in-buffer "foo( abc, |def)"
+                  'backward-one-funcarg
+                  "foo( |abc, def)"))
+
+(ert-deftest test-move-to-beginning-of-arg-complex ()
+  (test-in-buffer "foo(123, yyy=\"a,b\"|)"
+                  'backward-one-funcarg
+                  "foo(123, |yyy=\"a,b\")"))
+
+(ert-deftest test-move-to-previous-arg-complex ()
+  (test-in-buffer "foo(123, |yyy=\"a,b\")"
+                  'backward-one-funcarg
+                  "foo(|123, yyy=\"a,b\")"))
+
+
+;; forward-funcarg
+
+(ert-deftest test-forward-funcarg-one ()
+  (test-in-buffer "foo(|abc, def)"
+                  '(lambda () (forward-funcarg 1))
+                  "foo(abc|, def)"))
+
+(ert-deftest test-forward-funcarg-two ()
+  (test-in-buffer "foo(|abc, def)"
+                  '(lambda () (forward-funcarg 2))
+                  "foo(abc, def|)"))
+
+(ert-deftest test-forward-funcarg-negative ()
+  (test-in-buffer "foo(abc, def|)"
+                  '(lambda () (forward-funcarg -2))
+                  "foo(|abc, def)"))
+
+; XXX test when arg is larger than number of function params to move through
+
+;; transpose-funcarg
+
+(ert-deftest test-transpose-simple-forward ()
+  (test-in-buffer "foo(|abc, def)"
+                  '(lambda () (transpose-funcarg 1))
+                  "foo(def, abc|)"))
+
+(ert-deftest test-transpose-simple-forward-2 ()
+  (test-in-buffer "foo(a|bc, def)"
+                  '(lambda () (transpose-funcarg 1))
+                  "foo(def, abc|)"))
+
+(ert-deftest test-transpose-simple-backward ()
+  (test-in-buffer "foo(abc, def|)"
+                  '(lambda () (transpose-funcarg -1))
+                  "foo(def|, abc)"))
+
+(ert-deftest test-transpose-simple-backward-2 ()
+  (test-in-buffer "foo(abc, |def)"
+                  '(lambda () (transpose-funcarg -1))
+                  "foo(|def, abc)"))
+
 
 (defun test-in-buffer (before func-to-test after)
   (with-temp-buffer
@@ -17,7 +125,15 @@
     (insert "|")
     (should (string= (plain-thing-at-point 'line) after))))
 
-(ert-deftest test1 ()
-  (test-in-buffer "foo(|abc, def)"
-                  'forward-one-funcarg
-                  "foo(abc|, def)"))
+(defun plain-thing-at-point (thing-type)
+  "Like thing-at-point, but strip out any text properties"
+  (let ((thing (thing-at-point thing-type)))
+      (set-text-properties 0 (length thing) nil thing)
+      thing))
+
+; XXX - hack
+(defun run-tests-in-this-file ()
+  (interactive)
+  (ert-delete-all-tests)
+  (eval-buffer)
+  (ert t))
