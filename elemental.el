0fbdb3995b0f137595de8a0bb9b4795ff549e4c3
Menno Smits <menno@freshfoo.com>
2012-01-26 23:53:19 +0000

    documentation update - not complete

--- elemental.el
+++ elemental.el
@@ -1,3 +1,61 @@
+;; elemental.el -- functions for intelligently jumping between and
+;; transposing list/tuple/dictionary/function-parameter
+;; elements. These functions are mainly useful when editing software
+;; source code.
+;;
+;; Copyright (C) 2012 Menno Smits
+;;
+;; Author: Menno Smits <menno@freshfoo.com>
+;; Version: 0.1
+;; URL: https://bitbucket.org/mjs0/elemental/
+;;
+;; This file is not part of GNU Emacs.
+
+;; Description:
+;;
+;; It is difficult to efficiently navigate and manipulate list
+;; elements and function parameter lists, especially when string
+;; literals and nested function calls are involved. The functions in
+;; this module make this process easier.
+;;
+;; The main functions of interest are: forward-one-element,
+;; backward-one-element, forward-element and transpose-element.
+;;
+;; Given a source code line, here's how the point moves with success
+;; calls to forward-element:
+;;     something(|123, "some, string", fn(a, b))
+;;     something(123|, "some, string", fn(a, b))
+;;     something(123, "some, string"|, fn(a, b))
+;;     something(123, "some, string", fn(a, b)|)
+
+;; XXX finish
+
+;; Installation:
+;;
+
+
+
+;; Limitations:
+
+
+;; License:
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+
 (defun forward-one-element ()
   (interactive)
   (unless (outside-parens?)
